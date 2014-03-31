00faef35616f6b94e1fd368ccb5277b0a6147e92
Fengming Sun <s@sfmblog.cn>
2014-03-31 14:10:13 +0800

    release by ec3s/angular-adminui@6611cdbcbcd0d7e97f97ad9d7a1b6695b9671189

--- scripts/angular-adminui.js
+++ scripts/angular-adminui.js
@@ -555,7 +555,7 @@ angular.module('ntd.directives').directive('nanoScrollbar', [
             });
           } else {
             var url = $(et).attr('href');
-            $('#bs2').attr('href', 'http://ec3s.github.io/angular-adminui/' + url);
+            $('#bs3').attr('href', 'http://ec3s.github.io/adminui-3.0/' + url);
           }
         });
       }
@@ -1531,4 +1531,113 @@ angular.module('ntd.directives').directive('nanoScrollbar', [
     });
   };
   ng.module('ntd.directives').directive('checkboxGroup', [CheckboxGroup]);
+}(angular));
+(function (ng) {
+  'use strict';
+  var pagination = {
+      config: {
+        directionLinks: true,
+        previousText: '\xab',
+        nextText: '\xbb',
+        total: 1,
+        size: 5,
+        page: 1,
+        pageKey: 'page',
+        rotate: false
+      },
+      updateConfig: function (pageInfo) {
+        pageInfo = ng.extend(this.config, pageInfo);
+      },
+      noPrevious: function () {
+        return this.config.page === 1;
+      },
+      noNext: function () {
+        return this.config.page === this.config.total;
+      },
+      isActive: function (page) {
+        return parseInt(this.config.page) === parseInt(page);
+      },
+      makePage: function (number, text, isActive, isDisabled) {
+        return {
+          number: number,
+          text: text,
+          active: isActive,
+          disabled: isDisabled
+        };
+      },
+      getPages: function () {
+        var pages = [];
+        if (this.config.total <= 1) {
+          return;
+        }
+        var startPage = 1, endPage = this.config.total;
+        var isMaxSized = this.config.size && this.config.size < this.config.total;
+        if (isMaxSized) {
+          startPage = Math.max(this.config.page - Math.floor(this.config.size / 2), 1);
+          endPage = startPage + this.config.size - 1;
+          if (endPage > this.config.total) {
+            endPage = this.config.total;
+            startPage = endPage - this.config.size + 1;
+          }
+        }
+        for (var number = startPage; number <= endPage; number++) {
+          if (number == 1 || number == this.config.total) {
+            continue;
+          }
+          var page = this.makePage(number, number, this.isActive(number), false);
+          pages.push(page);
+        }
+        if (isMaxSized && !this.config.rotate) {
+          if (startPage > 1) {
+            var previousPageSet = this.makePage(startPage - 1, '...', false, false);
+            pages.unshift(previousPageSet);
+          }
+          if (endPage < this.config.total) {
+            var nextPageSet = this.makePage(endPage + 1, '...', false, false);
+            pages.push(nextPageSet);
+          }
+        }
+        var firstPage = this.makePage(1, 1, this.isActive(1), false);
+        pages.unshift(firstPage);
+        var lastPage = this.makePage(this.config.total, this.config.total, this.isActive(this.config.total), false);
+        pages.push(lastPage);
+        if (pagination.config.directionLinks) {
+          var previousPage = this.makePage(this.config.page - 1, this.config.previousText, false, this.noPrevious());
+          pages.unshift(previousPage);
+          var nextPage = this.makePage(this.config.page + 1, this.config.nextText, false, this.noNext());
+          pages.push(nextPage);
+        }
+        return pages;
+      },
+      selectPage: function ($location, page) {
+        if (!this.isActive(page) && page > 0 && page <= this.config.total) {
+          this.config.page = page;
+          var searchOpt = $location.search();
+          searchOpt[this.config.pageKey] = page;
+          $location.search(searchOpt).replace();
+        }
+      },
+      render: function ($scope) {
+        $scope.pages = this.getPages();
+      }
+    };
+  var AdminPageDirective = function ($location) {
+    return {
+      restrict: 'A',
+      template: '<ul class="pagination">\n' + '<li ng-repeat="page in pages"' + ' ng-class="{active: page.active, disabled: page.disabled}">' + '<a ng-click="selectPage(page.number)">{{page.text}}</a></li>\n' + '</ul>\n',
+      replace: true,
+      scope: { pageInfo: '=ngModel' },
+      link: function (scope, element, attrs) {
+        pagination.updateConfig(scope.pageInfo);
+        scope.$watch('pageInfo.page', function (value) {
+          pagination.render(scope);
+        }, true);
+        scope.selectPage = ng.bind(pagination, pagination.selectPage, $location);
+      }
+    };
+  };
+  ng.module('ntd.directives').directive('adminuiPagination', [
+    '$location',
+    AdminPageDirective
+  ]);
 }(angular));
