ceac5b7036080fd17ec94e774755ca34cdd19baf
Fengming Sun <s@sfmblog.cn>
2014-05-22 13:47:45 +0800

    release by ec3s/angular-adminui@1db0996b7712d81ab8c40d5d1bd2a18a0c2f8c26

--- scripts/angular-adminui.js
+++ scripts/angular-adminui.js
@@ -1385,7 +1385,7 @@ angular.module("ntd.directives").directive("nanoScrollbar", [ "$timeout", functi
                     }
                     initOptions();
                     changeSelect();
-                });
+                }, true);
                 var initOptions = function() {
                     baseLevels = [];
                     scope.values = [];
@@ -2265,9 +2265,99 @@ angular.module("ntd.directives").directive("nanoScrollbar", [ "$timeout", functi
     ng.module("ntd.directives").directive("adminuiSwitcher", [ "$timeout", SwitcherDirective ]);
 })(angular);
 
+(function(ng, app) {
+    "use strict";
+    var List = function($timeout, selectBox, elem, scope) {
+        this.items = [];
+        this.options = [];
+        this.$timeout = $timeout;
+        this.selectBox = selectBox;
+        this.selectBox.bind("change", {
+            scope: scope
+        }, this.change.bind(this));
+        $timeout(function() {
+            ng.forEach(selectBox.children("option"), function(option, index) {
+                var optionEl = ng.element(option);
+                if ($.trim(optionEl.text()) != "") {
+                    this.options.push(optionEl);
+                    this.items.push({
+                        text: optionEl.text(),
+                        value: optionEl.attr("value"),
+                        selected: option.selected,
+                        index: index
+                    });
+                }
+            }, this);
+            $timeout(function() {
+                elem.find("li").bind("click", function(e) {
+                    var index = elem.find("li").index(e.target);
+                    ng.forEach(this.options, function(option, optionIndex) {
+                        if (optionIndex == index) {
+                            if (scope.multiple === true) {
+                                option.get(0).selected = !option.get(0).selected;
+                            } else {
+                                option.get(0).selected = true;
+                            }
+                            option.change();
+                        }
+                    });
+                }.bind(this));
+            }.bind(this));
+        }.bind(this));
+    };
+    List.prototype.change = function(e) {
+        var selectedIndex = [];
+        ng.forEach(this.selectBox.find("option:selected"), function(option) {
+            selectedIndex.push(this.selectBox.find("option").index(option));
+        }, this);
+        ng.forEach(this.items, function(item) {
+            if (e) {
+                e.data.scope.$apply(function() {
+                    item.selected = selectedIndex.indexOf(item.index) !== -1;
+                });
+            } else {
+                item.selected = selectedIndex.indexOf(item.index) !== -1;
+            }
+        }, this);
+    };
+    var ListDirective = function($compile, $timeout, $parse) {
+        return {
+            restrict: "A",
+            scope: true,
+            replace: true,
+            templateUrl: "templates/adminui-list.html",
+            link: function(scope, elem, attrs) {
+                var list = null;
+                var options = attrs["ngOptions"];
+                var NG_OPTIONS_REGEXP = new RegExp("^\\s*([\\s\\S]+?)(?:\\s+as\\s+([\\s\\S]+?))" + "?(?:\\s+group\\s+by\\s+([\\s\\S]+?))?\\s+for\\s+" + "(?:([\\$\\w][\\$\\w]*)|(?:\\(\\s*([\\$\\w][\\$\\w]*)\\s*,\\s*" + "([\\$\\w][\\$\\w]*)\\s*\\)))\\s+in\\s+" + "([\\s\\S]+?)(?:\\s+track\\s+by\\s+([\\s\\S]+?))?$");
+                var optionsMatch = options.match(NG_OPTIONS_REGEXP);
+                var optionModelName = optionsMatch[7];
+                var selectBox = ng.element("<select>").attr("ng-options", options).attr("ng-change", attrs["ngChange"]).attr("ng-model", attrs["ngModel"]).append(ng.element("<option>"));
+                scope.multiple = $parse(attrs["multiple"])(scope);
+                if (scope.multiple === true) {
+                    selectBox.attr("multiple", true);
+                }
+                selectBox = $compile(selectBox)(scope);
+                scope.$watch(attrs["ngModel"], function(value, oldValue) {
+                    if (value !== oldValue) {
+                        scope.$parent[attrs["ngModel"]] = value;
+                        list.change();
+                    }
+                }, true);
+                scope.$watch(optionModelName, function(value, oldValue) {
+                    list = new List($timeout, selectBox, elem, scope);
+                    scope.listItems = list.items;
+                }, true);
+            }
+        };
+    };
+    app.directive("adminuiList", [ "$compile", "$timeout", "$parse", ListDirective ]);
+})(angular, angular.module("ntd.directives"));
+
 angular.module("ntd.directives").run([ "$templateCache", function($templateCache) {
     "use strict";
     $templateCache.put("templates/adminui-frame.html", '<nav class="navbar navbar-inverse navbar-fixed-top" role=navigation><div class=container-fluid><div class=navbar-header><button class=navbar-toggle type=button data-toggle=collapse data-target=.bs-navbar-collapse><span class=sr-only>Toggle navigation</span> <span class=icon-bar></span> <span class=icon-bar></span> <span class=icon-bar></span></button> <a class=navbar-brand href="../"></a></div><div class="collapse navbar-collapse bs-navbar-collapse"><ul class="nav navbar-nav main-nav"><li data-ng-repeat="nav in navigation" data-ng-class="{true: \'active\', false: \'\'}[nav.children != null]"><a data-ng-href={{nav.url}} data-ng-show=nav.show>{{nav.name}}</a><ul class=sub-navbar data-ng-if="nav.children != null" data-ng-class="{false: \'no-parent\'}[nav.show]"><li data-ng-repeat="subnav in nav.children" data-ng-show=subnav.show data-ng-class="{true: \'active\', false: \'\'}[isSelected(subnav)]"><a data-ng-click=selectNav(subnav) data-ng-href={{subnav.url}}>{{subnav.name}}</a></li></ul></li></ul><ul class="nav navbar-nav navbar-right"><li class=dropdown><a data-ng-show=isMessageBoxShow class=dropdown-toggle href=# data-toggle=dropdown><span class=badge data-ng-show="messages.length > 0">{{messages.length}}</span> <i class="glyphicon glyphicon-inbox"></i></a><ul data-ng-show=isMessageBoxShow class=dropdown-menu><li data-ng-repeat="message in messages"><a href=#>{{message.title}}</a></li><li class=divider></li><li><a href=#><i class="glyphicon glyphicon-chevron-right pull-right"></i> 查看全部</a></li></ul></li><li data-ng-hide=noSideMenu data-ng-class="{true: \'active\', false: \'\'}[isSubMenuShow]"><a href=javascript:; data-ng-click=toggleSubMenu($event)><i class="glyphicon glyphicon-list"></i></a></li><li data-ng-show="userInfo.username != null" class=dropdown><a class=dropdown-toggle href=# data-toggle=dropdown>您好，{{userInfo.username}}<b class=caret></b></a><ul class=dropdown-menu><li class=user-information><img data-ng-src={{userInfo.avatar}} alt=user_avatar><div class="user-content muted">{{userInfo.username}}</div></li><li class=divider></li><li><a data-ng-click=changePwd($event) href=#><i class="glyphicon glyphicon-lock"></i> 修改密码</a></li><li><a data-ng-click=logout($event) href=#><i class="glyphicon glyphicon-off"></i> 退出</a></li></ul></li><li class=dropdown data-ng-show="userInfo.username != null && accountHost != null "><a class=dropdown-toggle href=# data-toggle=dropdown>常用 <b class=caret></b></a><ul class=dropdown-menu><li data-ng-repeat="menu in commonMenus"><a data-ng-href={{menu.link}}>{{menu.name}}</a></li><li data-ng-show="commonMenus.length <= 0" class=none-info>暂无常用菜单</li><li class=divider></li><li><a href=javascript:; data-ng-click=addCommonMenu()>添加为常用</a></li><li><a data-ng-href={{accountHost}}/#/menus>管理常用菜单</a></li></ul></li></ul></div></div><nav class=sub-navbar-back data-ng-show=hasSubNav></nav></nav><div class=container-fluid><div class="row fix-row"><div class="col-md-2 container-fluid" data-ng-show=hasSideMenu><div data-ng-show=isSubMenuShow class=row><div class="col-md-2 affix side-nav"><h4>{{sideMenuName}}</h4><ul class=side-nav-menu><li data-ng-repeat="sidemenu in menu" data-ng-class="{true: \'active\', false: \'\'}[isSelected(sidemenu)]"><a data-ng-href={{sidemenu.url}} data-ng-class="{true: \'has-sub-menu\', false: \'\'}[sidemenu.children != null]" data-ng-click="selectMenu(sidemenu, $event)"><i class="pull-right glyphicon glyphicon-chevron-down" data-ng-show="sidemenu.children != null"></i>{{sidemenu.name}}</a><ul data-ng-if="sidemenu.children != null"><li data-ng-repeat="subsidemenu in sidemenu.children" data-ng-class="{true: \'active\', false: \'\'}[isSelected(subsidemenu)]"><a data-ng-click="selectMenu(subsidemenu, $event)" data-ng-href={{subsidemenu.url}}>{{subsidemenu.name}}</a></li></ul></li></ul></div></div></div><div data-ng-class="{true: \'col-md-offset-2\', false: \'\'}[isSubMenuShow && hasSideMenu]" class="message-container flash-alert"></div><div data-ng-class="{true: \'col-md-offset-2\', false: \'\'}[isSubMenuShow && hasSideMenu]" class="message-container notice"></div><div data-ng-class="{true: \'col-md-10\', false: \'col-md-12\'}[isSubMenuShow && hasSideMenu]" data-ng-transclude=""></div><div data-adminui-load-backdrop="" class=load-backdrop style=display:none><div class=splash><img class=loading src=images/ajax-loader.gif alt=加载中></div></div></div></div>');
+    $templateCache.put("templates/adminui-list.html", '<ul class=adminui-list-container><li data-ng-repeat="item in listItems" data-ng-class="{true: \'selected\'}[item.selected]">{{item.text}}</li></ul>');
     $templateCache.put("templates/adminui-switcher.html", "<div class=adminui-switcher-container><div class=adminui-switcher-inner><div class=on-label data-ng-bind-html=onLabel></div><div class=divider></div><div class=off-label data-ng-bind-html=offLabel></div><div class=ng-hide><input type=checkbox data-ng-model=model></div></div></div>");
     $templateCache.put("templates/checkbox-group.html", "<div class=\"dropdown dropdown-checkbox-group\"><label class=dropdown-toggle data-toggle=dropdown><input type=checkbox data-ng-click=toggleCheckedAll() data-ng-class=\"{'part': 'part-checked'}[status]\" data-ng-checked=\"{'all': true, 'part': true, 'none': false}[status]\">{{dataSource.name}} <b class=caret></b></label><ul class=dropdown-menu><li data-ng-repeat=\"checkbox in dataSource.checkboxGroup\"><label><input type=checkbox data-ng-model=checkbox.checked>{{checkbox.name}}</label></li><li data-ng-show=\"dataSource.checkboxGroup.length <= 0\"><label>无可选项目</label></li></ul></div>");
     $templateCache.put("templates/common-menu-dialog.html", '<div class=modal-header><h3>将当前地址加入常用菜单</h3></div><div class=modal-body data-ng-keypress=add($event)><form class=form-horizontal><div class=form-group><label class="col-sm-2 control-label" for="">地址</label><div class=col-sm-9><input class="form-control input-sm" data-ng-model=menu.link readonly></div></div><div class=form-group><label class="col-sm-2 control-label" for="">名称</label><div class=col-sm-9><input class="form-control input-sm" data-ng-model=menu.name></div></div></form></div><div class=modal-footer><button class="btn btn-primary" data-ng-click=add($event)>添加</button> <button class="btn btn-default" data-ng-click=cancel($event)>取消</button></div>');
